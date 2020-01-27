72b0ef32cddc9800fae64e89f6599217d6f3a1cb
Seweryn91 <s.talaj@gmail.com>
2020-01-27 17:03:01 +0100

    Update implementation

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
 
     public String fizzBuzz(int i) {
-        if (i == 3) return "Fizz";
+        if (i == 3 || i == 6) return "Fizz";
         else if (i == 5) return "Buzz";
         else return String.valueOf(i);
     }
